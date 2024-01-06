752854b6d746afef413d5fd87bdd52fabc79839b
jai <76526688+jai050803@users.noreply.github.com>
2024-01-06 12:34:55 +0530

    Update linear_search.java

--- linear_search.java
+++ linear_search.java
@@ -1,17 +1,1 @@
-public class linear_search{
-    public static int search(int[] numbers){
-        int maxnumber = numbers[0];
 
-        for (int i = 1; i< numbers.length ; i++){
-            if (numbers[i] > maxnumber){
-                maxnumber = numbers[i];
-            }
-        }
-        return maxnumber;
-    }
-    public static void main(String[] args) {
-        int[] number_list = {4,8,8,2,5,7,9,12};
-        int result = search(number_list);
-        System.out.println(result);
-    }
-}
